f4cd24bd30029b8d68c0ae46d03ca94b104cd594
Muhammet Emin Tufan <muhammedemintufan@gmail.com>
2023-05-19 11:12:02 +0300

    Profile screen details
    added employee informations on profile screen

--- frontend/src/Profile.jsx
+++ frontend/src/Profile.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import './style.css'
 
 function Profile() {
   return (
     <div>
-     
-    </div>
-  )
+      <h1>Profile</h1>
+    <main className="centered">
+      <div className="container">
+        <h2>Name: Mark</h2>
+        <h2>Surname: Otto</h2>
+        <h2>Salary: 2360$</h2>
+        <h2>Position: Admin</h2>
+      </div>
+    </main>
+  </div>
+);
 }
 
 export default Profile
